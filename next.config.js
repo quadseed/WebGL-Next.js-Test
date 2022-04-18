308ce04b98923f0cbed68c1865be787e479bd888
quadseed <contact@quadseed.net>
2022-04-18 16:19:32 +0900

    Edit: fix prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/WebGL-Next.js-Test" : "",
   basePath: process.env.GITHUB_ACTIONS ? "/WebGL-Next.js-Test" : "",
   trailingSlash: true,
 }
