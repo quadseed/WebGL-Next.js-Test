b728872fdb6aaaa721fe4e13e03f48714bf1281a
quadseed <contact@quadseed.net>
2022-04-18 16:21:48 +0900

    Edit: fix path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.GITHUB_ACTIONS ? "/WebGL-Next.js-Test" : "",
-  basePath: process.env.GITHUB_ACTIONS ? "/WebGL-Next.js-Test" : "",
+  assetPrefix: "/WebGL-Next.js-Test",
+  basePath: "/WebGL-Next.js-Test",
   trailingSlash: true,
 }
 
